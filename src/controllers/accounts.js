de48ace702953c5657032a12288c2324bf6d0992
Grzegorz Krawiec <krawiec.grzegorz1@gmail.com>
2021-01-04 01:09:48 +0100

    login

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -8,9 +8,7 @@ module.exports = {
 
     createAccount: async function (req, res) {
         try {
-            const {
-                error
-            } = validate(req.body);
+            const { error } = validate(req.body);
             if (error) return res.status(400).send(error.details[0].message);
             
             account = new Account(req.body);
@@ -22,6 +20,21 @@ module.exports = {
         } catch (e) {
             console.log(e);
             return res.status(404).send(e);
+        }
+    },
+    login: async function (req, res) {
+        try {
+            let account = await Account.findOne({ email: req.body.email });
+            if (!account) return res.status(404).send('Invalid email or password.');
+
+            if (req.body.password !== account.password) {
+                return res.status(404).send('Invalid email or password.')
+            }
+            const token = jwt.sign(account.email, process.env.ACCESS_TOKEN_SECRET);
+            res.status(200).send({token});
+        } catch (ex) {
+            console.log(ex);
+            return res.status(404).send(ex)
         }
     },
     getAccounts: async function (req, res) {
--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const accountsController = require('../controllers/accounts');
+const autho = require('../middleware/autho');
 
 router.route('/').post(accountsController.createAccount);
-router.route('/').get(accountsController.getAccounts);
+router.route('/').get(autho).get(accountsController.getAccounts);
+router.route('/login').post(accountsController.login);
+
 
 module.exports = router;
